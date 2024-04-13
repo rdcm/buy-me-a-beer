c1336570f73090b2c2752ccc2e26fa02764ef719
rdcm <rdcm@protonmail.ch>
2024-04-13 03:00:14 +0300

    nice button added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,7 @@
 import { World } from "./world.ts";
-import { onBuy, onRevoke } from "./handlers.ts";
+import { onBuy } from "./handlers.ts";
 
 document.querySelector("#buy")!.addEventListener("click", onBuy);
-document.querySelector("#revoke")!.addEventListener("click", onRevoke);
 
 (async () => {
   await new World(100, "#1099bb", [
