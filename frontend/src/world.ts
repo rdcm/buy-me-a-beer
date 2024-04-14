26d7a45984f90619c4bb6782aac101a635a65744
rdcm <rdcm@protonmail.ch>
2024-04-15 02:24:56 +0300

    tuples removed from world

--- frontend/src/world.ts
+++ frontend/src/world.ts
@@ -1,14 +1,19 @@
 import { Assets, Sprite, Application, Ticker } from "pixi.js";
 import { randomNumber } from "./utils";
 
+interface Bottle {
+  sprite: Sprite;
+  direction: number;
+}
+
 export class World {
   private readonly paths: string[];
-  private readonly bottles: number;
+  private readonly count: number;
   private readonly background: string;
   private readonly app: Application = new Application();
 
-  constructor(bottles: number, background: string, paths: string[]) {
-    this.bottles = bottles;
+  constructor(count: number, background: string, paths: string[]) {
+    this.count = count;
     this.background = background;
     this.paths = paths;
   }
@@ -16,30 +21,30 @@ export class World {
   public async Run() {
     await this.app.init({ background: this.background, resizeTo: window });
     document.body.appendChild(this.app.canvas);
-    const sprites = await this.fillGrid();
-    this.app.ticker.add((timer) => this.onTick(timer, sprites));
+    const bottles = await this.fillGrid();
+    this.app.ticker.add((timer) => this.onTick(timer, bottles));
   }
 
-  private async fillGrid(): Promise<[Sprite, number][]> {
-    const sprites: [Sprite, number][] = [];
-    for (let index = 0; index < this.bottles; index++) {
+  private async fillGrid(): Promise<Bottle[]> {
+    const bottles: Bottle[] = [];
+    for (let index = 0; index < this.count; index++) {
       const index = randomNumber(100_000_000) % this.paths.length;
-      const pair = await this.loadSprite(this.paths[index]);
+      const bottle = await this.loadSprite(this.paths[index]);
 
-      this.setupSprite(pair[0]);
-      this.spawn(screen.width, screen.height, pair[0]);
-      sprites.push(pair);
+      this.setupSprite(bottle.sprite);
+      this.spawn(screen.width, screen.height, bottle.sprite);
+      bottles.push(bottle);
     }
 
-    return sprites;
+    return bottles;
   }
 
-  private async loadSprite(path: string): Promise<[Sprite, number]> {
+  private async loadSprite(path: string): Promise<Bottle> {
     await Assets.load(path);
     const sprite = Sprite.from(path);
     this.app.stage.addChild(sprite);
     const direction = Math.floor(Math.random() * 2) == 0 ? 1 : -1;
-    return [sprite, direction];
+    return { sprite, direction };
   }
 
   private setupSprite(sprite: Sprite) {
@@ -52,10 +57,10 @@ export class World {
     sprite.y = Math.floor(Math.random() * height);
   }
 
-  private onTick(time: Ticker, sprites: [Sprite, number][]) {
-    for (let index = 0; index < sprites.length; index++) {
-      const pair = sprites[index];
-      this.rotate(pair[0], time, pair[1]);
+  private onTick(time: Ticker, bottles: Bottle[]) {
+    for (let index = 0; index < bottles.length; index++) {
+      const bottle = bottles[index];
+      this.rotate(bottle.sprite, time, bottle.direction);
     }
   }
 
